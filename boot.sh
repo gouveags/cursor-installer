3b32d9357a6e4ea2a8c2b5fcb0d94a005e5c24e3
gouveags <gouswat@gmail.com>
2025-06-05 04:15:39 -0300

    Enhance boot script to check for apt availability and handle unsupported distros

--- boot.sh
+++ boot.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Exit immediately if a command exits with a non-zero status
 set -e
 
 cat << 'EOF'
@@ -40,11 +41,16 @@ echo -e "=> No need to be frustrated anymore, cursor-installer is here to help! 
 
 echo -e "Beginning installation process. Press Ctrl+C to cancel at any time.\n"
 
-sudo apt-get update >/dev/null
-sudo apt-get install -y git >/dev/null
+if command -v apt >/dev/null; then
+	sudo apt-get update >/dev/null
+	sudo apt-get install -y git >/dev/null
+else
+  echo -e "/n❌ Unsupported distro (need apt). Exiting."; exit 1
+fi
 
 echo "Cloning cursor-installer..."
 rm -rf ~/.local/share/cursor-installer
+mkdir -p ~/.local/share/cursor-installer
 git clone https://github.com/gouveags/cursor-installer.git ~/.local/share/cursor-installer >/dev/null
 if [[ $BRANCH_REF != "main" ]]; then
 	cd ~/.local/share/cursor-installer
